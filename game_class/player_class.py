dfc1a7cd894131aa6e7b8a27babe07c26bb489f1
JeanErick-Voigt <voigtjeanerick@gmail.com>
2017-09-02 10:56:34 -0500

    Update player_class.py

--- game_class/player_class.py
+++ game_class/player_class.py
@@ -2,12 +2,15 @@ from game_class.character_class import Character
 
 
 class Player(Character):
+	#player class inherits from parent class which is character and super init done to 
+	#get the values already in character class
 	def __init__(self, name, hp, max_hp):
 		super().__init__(name, hp, max_hp)
 		self._loot = {}
 	
 	
 	@property
+	#function in order to verify if loot is there
 	def loot(self):
 		if not self._loot:
 			print("empty bag")
@@ -17,6 +20,7 @@ class Player(Character):
 				print("contents of loot: %s" %key, sep="\n")
 
 	@loot.setter
+	#function in order to set loot
 	def loot(self, item):
 		if not self._loot:
 			self._loot[item] = 1
@@ -32,6 +36,7 @@ class Player(Character):
 		return self._loot
 
 	def use_item(self, item):
+		#function used in order to use_item
 		if item not in self._loot:
 			print("You do not have any %s" %item)
 		else:
@@ -40,6 +45,7 @@ class Player(Character):
 		return(self._loot[item]) 
 
 	def show_contents(self):
+		#function to show contents of bag
 		print("Contents of bag")
 		for key in self._loot:
 			print("Item: {0}, Amount: {1}".format(key, self._loot.get(key)))
